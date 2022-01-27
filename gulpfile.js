cd22ebd8db968817c463f907c5ca41847acef56f
Block <1a1@mail.com>
2022-01-27 12:52:46 +0300

    Start Header

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ function cleanDist(){
 
 function styles() {
 	return src('app/scss/style.scss')
-	.pipe(scss({outputStyle:'compressed'}))
+	.pipe(scss({outputStyle:'expanded'}))
 	.pipe(concat('style.min.css')) 
 	.pipe(autoprefixer({
 		overrideBrowserslist: ['last 10 version'],
