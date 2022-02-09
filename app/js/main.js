5b840bcd8baa1a25e61e0aacc3f361da21a46333
Block <1a1@mail.com>
2022-02-09 19:29:59 +0300

    dz13 v3

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,48 @@
-$(document).ready(function(){
-  $('.top-slider__list').slick({
-	arrows: true,
-  });
+$(function(){
+	$('.top-slider__list').slick({
+		arrows: true,
+		prevArrow: '<button class="btn-prev" type="button"><div class="sr-only">следующий слайд</div></button>',
+		nextArrow: '<button class="btn-next" type="button"><div class="sr-only">следующий слайд</div></button>',
+	});
+
   
-  $('.brends__list').slick({
+	$('.brends__list').slick({
 	slidesToShow: 6,
 	slidesToScroll: 1,
 	arrows: false,
-  });
-});
-
-$(function(){
-	$('.dropdown__btn').on('click', function() {
-		$('.dropdown__btn').toggleClass('dropdown__btn--active');
-		$('.dropdown__list').toggleClass('dropdown__list--active');
 	});
 
-	// $('.product-card__btn').on('click', function() {
-	// 	$('.product-card__btn').toggleClass('product-card__btn--light');
-	
-	// }); - Хотел сделать скрипт, что бы когда нажимаешь на сердечко оно оставалось с желтым цветом
+
+	$('.dropdown__btn').on('click', function() {
+		$('.dropdown__btn').toggleClass('dropdown__btn--active');
+		$('.dropdown__list').slideToggle();
+
+		let btnCategories = $(".dropdown__btn"); // указываем кнопку
+		let categories = $(".dropdown__list");
+
+		$(document).on('click', function (e) { // событие клика по веб-документу
+			if ( ! btnCategories.is(e.target) && btnCategories.has(e.target).length === 0 &&
+					// если клик был не по нашему блоку
+					! categories.is(e.target) && categories.has(e.target).length === 0
+					// и не по его дочерним элементам
+					) {
+				categories.fadeOut();
+				 // скрываем его
+				}
+			});
+	});
 
 	var containerEl1 = document.querySelector('[data-ref="mix-1"]');
-    var containerEl2 = document.querySelector('[data-ref="mix-2"]');
+	var containerEl2 = document.querySelector('[data-ref="mix-2"]');
 
 	var config = {
 		controls: {
-		  scope: 'local'
+			scope: 'local'
 		}
-	  };
-	 
+		};
+		
 	var mixer1 = mixitup(containerEl1, config);
 	var mixer2 = mixitup(containerEl2, config);	
-});
+
+	});
 
