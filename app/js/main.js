730d71f703d6cdc7b4cd20d99968302fabd746e1
Block <1a1@mail.com>
2022-03-02 10:41:57 +0300

    Добавлено 2 страницы HTML

--- app/js/main.js
+++ app/js/main.js
@@ -37,12 +37,13 @@ $(function () {
 		]
 	});
 
-	$('.dropdown__btn').on('click', function () {
-		$('.dropdown__btn').toggleClass('dropdown__btn--active');
-		$('.dropdown__list').slideToggle();
 
-		let btnCategories = $(".dropdown__btn"); // указываем кнопку
-		let categories = $(".dropdown__list");
+	$('.tape__btn').on('click', function () {
+		$('.tape__btn').toggleClass('tape__btn--active');
+		$('.tape__list').slideToggle();
+
+		let btnCategories = $(".tape__btn"); // указываем кнопку
+		let categories = $(".tape__list");
 
 		$(document).on('click', function (e) { // событие клика по веб-документу
 			if (!btnCategories.is(e.target) && btnCategories.has(e.target).length === 0 &&
@@ -50,13 +51,50 @@ $(function () {
 				!categories.is(e.target) && categories.has(e.target).length === 0
 				// и не по его дочерним элементам
 			) {
-				categories.fadeOut();
+				categories.slideUp();
 				// скрываем его
 			}
 		});
 
 	});
 
+
+	$('.categories-btn').on('click', function () {
+		$('.categories-btn').toggleClass('categories-btn--open');
+
+		$('.categories__list').slideToggle();
+	});
+
+	$('.offers-btn').on('click', function () {
+		$('.offers-btn').toggleClass('offers-btn--open');
+		$('.offers__list').slideToggle();
+	});
+
+	$('.firm-btn ').on('click', function () {
+		$('.firm-btn').toggleClass('firm-btn--open');
+		$('.firm__list').slideToggle();
+	});
+
+	$('.range-btn').on('click', function () {
+		$('.range-btn').toggleClass('range-btn--open');
+		$('.range__items').slideToggle();
+	});
+
+	// Сетка продуктов Grid products 
+
+	$('.shop__filter-btn').on('click', function () {
+		$('.shop__filter-btn').removeClass('shop__filter-btn--active');
+		$(this).addClass('shop__filter-btn--active');
+	});
+
+	$('.button-list').on('click', function (){
+		$('.shop__list').addClass('shop__list--column');
+	});
+
+	$('.button-grid').on('click', function (){
+		$('.shop__list').removeClass('shop__list--column');
+	});
+	
 	var containerEl1 = document.querySelector('[data-ref="mix-1"]');
 	var containerEl2 = document.querySelector('[data-ref="mix-2"]');
 
@@ -74,7 +112,7 @@ $(function () {
 });
 
 
-$('.users__btn-search').on('click', function () {
+$('.users__link--search').on('click', function () {
 	$('.form-search').toggleClass('form-search--active');
 });
 
@@ -89,3 +127,64 @@ $('.mobile__close-menu').on('click', function () {
 	$('body').removeClass('lock');
 	$('.overlay').removeClass('overlay--on');
 });
+
+$(function () {
+	const rangeSlider = document.getElementById('range-slider');
+
+	noUiSlider.create(rangeSlider, {
+	start: [1000, 3000],
+	connect: true,
+	step: 1,
+	range: {
+		'min': [100],
+		'max': [5000]
+		}
+	});
+
+	const input0 = document.getElementById('input-range-0');
+	const input1 = document.getElementById('input-range-1');
+
+	const inputs = [input0, input1]
+
+	rangeSlider.noUiSlider.on('update', function(values, handle) {
+		inputs [handle].value = Math.round (values[handle]);
+	});
+
+	const setRangeSlider = (i, value) => {
+		let arr = [null, null];
+		arr[i] = value;
+
+		console.log(arr);
+
+		rangeSlider.noUiSlider.set(arr);
+	};
+
+	inputs.forEach((el, index) => {
+		el.addEventListener('change', (e) => {
+			console.log(index);
+			setRangeSlider (index, e.currentTarget.value);
+		});
+	});
+});
+
+$(function () {
+	$('.sel1, .counter-iter').styler();
+
+}); 
+
+
+$(".products__star").rateYo({
+	starWidth: "16px",
+	normalFill: "#C1C1C1",
+	// starSvg: "<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">"+
+	// 			"<path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379"+
+	// 					" 4.246-3.611-2.625-3.612 2.625"+
+	// 					" 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833"+
+	// 					" 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388"+
+	// 					" 7.416 5.388-2.833-8.718"+
+	// 					" 7.417-5.389h-9.167l-2.833-8.718z"></path>"
+	// 		"</svg>"
+});
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ function images(){
 function browsersync(){
 	browserSync.init({
 		server: {
-			baseDir: "app/"
+			baseDir: "app/" 
 		},
 		notify: false
 	});
@@ -79,7 +79,7 @@ function styles() {
 	.pipe(concat('style.min.css')) 
 	.pipe(autoprefixer({
 		overrideBrowserslist: ['last 10 version'],
-		grid: true
+		grid: false
 	}))
 	.pipe(dest('app/css'))
 	.pipe(browserSync.stream());
@@ -91,6 +91,10 @@ function scripts(){
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/slick-carousel/slick/slick.js',
 		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+		'node_modules/nouislider/dist/nouislider.js',
+		'node_modules/rateyo/src/jquery.rateyo.js',
+		// 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
