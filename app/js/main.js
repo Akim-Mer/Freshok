869c2737d00f96929b5e1082c195568bf76fa3df
Block <1a1@mail.com>
2022-03-05 01:07:49 +0300

    Стилизовал UI компоненты

--- app/js/main.js
+++ app/js/main.js
@@ -170,21 +170,57 @@ $(function () {
 $(function () {
 	$('.sel1, .counter-iter').styler();
 
+	$('.products__images-box').slick ({
+		infinite: false,
+		arrows: true,
+		prevArrow: '<button class="products__btn-slide products__btn-slide--prev" type="button"><div class="sr-only">следующий слайд</div></button>',
+		nextArrow: '<button class="products__btn-slide products__btn-slide--next" type="button"><div class="sr-only">следующий слайд</div></button>',
+	});
+
+	$('.info__link').on('click', function(e){
+		e.preventDefault();
+		$('.info__link').removeClass('info__link--light');
+		$(this).addClass('info__link--light');
+
+		$('.tabs__item').removeClass('tabs__item--active');
+		$($(this).attr('href')).addClass('tabs__item--active');
+
+
+	});
+
+	$('.offers-slider__content').slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: true,
+		// prevArrow: '<button class="top-slider__btn top-slider__btn--prev" type="button"><div class="sr-only">следующий слайд</div></button>',
+		// nextArrow: '<button class="top-slider__btn top-slider__btn--next" type="button"><div class="sr-only">следующий слайд</div></button>',
+		prevArrow: '<button class="offers-slider__btn offers-slider__btn--prev" type="button"> <span class="sr-only">предыдущий слайд</span> </button>',
+		nextArrow: '<button class="offers-slider__btn offers-slider__btn--next" type="button"> <span class="sr-only">следующий слайд</span> </button>',
+	});
 }); 
 
 
-$(".products__star").rateYo({
+$(".star").rateYo({
 	starWidth: "16px",
-	normalFill: "#C1C1C1",
-	// starSvg: "<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">"+
-	// 			"<path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379"+
-	// 					" 4.246-3.611-2.625-3.612 2.625"+
-	// 					" 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833"+
-	// 					" 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388"+
-	// 					" 7.416 5.388-2.833-8.718"+
-	// 					" 7.417-5.389h-9.167l-2.833-8.718z"></path>"
-	// 		"</svg>"
+	spacing: "6px",
+	starSvg: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0.0229731 6.16432C0.0780973 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64481L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141566 6.64478C0.0138168 6.52028 -0.0322151 6.334 0.0229731 6.16432Z" /></svg>`, //убирать fill
+	normalFill: "#C1C1C1", //фон
+	ratedFill: "#FFB800",  // цвет звезды
+
 });
 
 
+// var normalFill = $("#rateYo").rateYo("option", "normalFill"); //returns "#A0A0A0"
+
+// // Setter
+// $("#rateYo").rateYo("option", "normalFill", "#C1C1C1"); //returns a jQuery Element
+
+
+
+// // Getter
+// var ratedFill = $("#rateYo").rateYo("option", "ratedFill"); //returns "#E74C3C"
+ 
+// // Setter
+// $("#rateYo").rateYo("option", "ratedFill", "#FFB800"); //returns a jQuery Element
 
--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ function scripts(){
 		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
 		'node_modules/nouislider/dist/nouislider.js',
 		'node_modules/rateyo/src/jquery.rateyo.js',
-		// 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
